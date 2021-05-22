8be88e65dd11c7807430ab2f16cd7b3774c0d617
dark <473380917@qq.com>
2021-05-22 11:28:07 +0800

    webpack 支持 IE，但不完全支持。Promise 需要使用 第三方polyfill模拟实现Promise

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hi webpack')
+import b from './b'
+const c =
+    import ('./b')
+
+const hi = () => {
+    console.log(c)
+    console.log(b)
+    console.log(Promise.resolve('success'))
+}
+hi()
