3157430b57b4cf07a257d3c88a0a0e0040d5ff89
dark <473380917@qq.com>
2021-05-23 11:05:03 +0800

    js 支持 alias

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import aaa from './ts-demo.ts'
+import {y} from '@/a.js'
 import TsxDemo from './tsx-demo.tsx'
-
+console.log(y)
 const hi = () => {
     console.log(aaa);
     console.log(TsxDemo)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require('path')
 module.exports = {
     mode: 'production',
     plugins: [new ESLintPlugin({
         extensions:['.js','.jsx','.ts','.tsx']
     })],
+    resolve:{
+        alias:{
+            '@':path.resolve(__dirname,'./src')
+        }
+    },
     module: {
         rules: [{
             test: /\.[jt]sx$/,
