e1ec233df254200a2fdd4daec0b044b207d8bcc0
dark <473380917@qq.com>
2021-05-25 22:07:49 +0800

    禁用一些不必要的eslint 限制

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,20 @@ module.exports = {
     rules: {
         'react/jsx-uses-react': [2],
         'react/react-in-jsx-scope': [2],
-        
+        'max-len': 'off' // disables line length check
     },
     overrides: [{
         files: ['*.ts', '*.tsx'],
         parserOptions: {
-            project: './tsconfig.json',
+            project: './tsconfig.json'
         },
         extends: ['airbnb-typescript'],
         rules: {
             '@typescript-eslint/object-curly-spacing': [0],
             'import/prefer-default-export': [0],
-            'no-console': [0]
+            'no-console': [0],
+            'max-len': 'off', // disables line length check
+            '@typescript-eslint/comma-dangle':'off'
         }
     }]
 }
